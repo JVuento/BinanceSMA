d1e1fc5ec2c752c5cdf180368f9e47ecf10c5727
JVuento <jvuento@gmail.com>
2021-03-20 22:04:46 +0200

    templatesecrets edit

--- templatesecrets.py
+++ templatesecrets.py
@@ -1,3 +1,9 @@
 #Rename to secrets.py and update your secrets.
+#---------------------------------------------
+#Never give these to anyone else!!!!
+#Secrets.py is included in gitignore, still be sure not to update your keys etc to anywhere!!!
+
+API_KEY='KEY'
+API_SECRET='SECRET'
 
 
