676461706ee49a2ce7fe642e520b61ba4ff39a70
Jukka Vuento <jvuento@gmail.com>
2021-12-14 20:16:41 +0200

    setting

--- tsl_settings.py
+++ tsl_settings.py
@@ -2,5 +2,10 @@ VERSIO='tsl 0.1'
 TIEDOT = []
 COINS=[
   #enter coinst to be traded in order: alt, stable and percentage stoploss is made
-  ['FTM','BUSD',2]  
+  ['FTM','BUSD',2],
+  ['AVAX','BUSD',3],
+  ['ROSE','BUSD',3],
+  ['LRC','BUSD',3],
+  ['RLC','BUSD',3],
+  ['LIT','BUSD',3]
   ]
