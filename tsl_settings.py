d77c0030f44ad61e2fd62d1f24655d7ef4bb5e6c
Jukka Vuento <jvuento@gmail.com>
2022-01-03 10:48:57 +0200

    settings
    added amounts of alts

--- tsl_settings.py
+++ tsl_settings.py
@@ -2,11 +2,11 @@ VERSIO='tsl 0.1'
 TIEDOT = []
 COINS=[
   #enter coinst to be traded in order: alt, stable and percentage stoploss is made
-  #['FTM','BUSD',3],
-  #['AVAX','BUSD',3],
-  #['ROSE','BUSD',3],
-  #['LRC','BUSD',3],
-  #['RLC','BUSD',3],
-  #['LIT','BUSD',3]
-  ['WIN','USDT',3]
+  #['FTM','BUSD',3,152],
+  #['AVAX','BUSD',3,3],
+  #['ROSE','BUSD',3,861],
+  #['LRC','BUSD',3,149],
+  #['RLC','BUSD',3,92],
+  #['LIT','BUSD',3,88],
+  ['WIN','USDT',9,108000]
   ]
